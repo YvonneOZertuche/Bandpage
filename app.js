c4815e4e4b8af59f953675bbbc88a5201851328d
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-21 19:20:04 -0500

    Added comments to code

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const express = require('express')
 const app = express()
 let port = 3000
 
-app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(require('./routes/index')) //route for main page - display all cover images
+app.use(require('./routes/albums'))//route to individual album info
+// app.use(require('./routes/contact'))
+//app.use(require('./routes/chat'))
 
-app.use(require('./routes/index'))
-app.use(require('./routes/albums'))
-
-
+//Default route
 app.get('/', (req,res) => {
   res.send('!Hola Mundo!')
 })
 
-
-
-
-
-
-
-
-
-
-
+//Checking to see that server is working
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`)
 })
